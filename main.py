c4d37270380ddd0c082da0d005d6216963f50f90
macbookair <cr7dima777@gmail.com>
2025-06-05 18:59:09 +0300

    v1.2.1

--- main.py
+++ main.py
@@ -20,75 +20,75 @@ XUI_PASS = os.getenv("XUI_PASSWORD")
 HEADERS = {"Content-Type": "application/json"}
 
 
-async def xui_login():
-    async with httpx.AsyncClient(base_url=XUI_URL, follow_redirects=True) as client:
-        resp = await client.post("/login", data={"username": XUI_USER, "password": XUI_PASS})
-        if "Set-Cookie" not in resp.headers:
+async def xui_login() -> dict:
+    async with httpx.AsyncClient(base_url=XUI_URL, follow_redirects=True) as temp_client:
+        resp = await temp_client.post("/login", data={"username": XUI_USER, "password": XUI_PASS})
+        if resp.status_code != 200 or "Set-Cookie" not in resp.headers:
             raise Exception("Не удалось авторизоваться в 3x-ui")
-        return client
-
+        cookies = temp_client.cookies
+        return cookies
 
 async def create_vless_user(telegram_id: int):
-    client = await xui_login()
-    user_uuid = str(uuid.uuid4())
-    user_tag = f"user_{telegram_id}"
-    short_id = uuid.uuid4().hex[:8]
+    cookies = await xui_login()
+    async with httpx.AsyncClient(base_url=XUI_URL, cookies=cookies) as client:
+        user_uuid = str(uuid.uuid4())
+        user_tag = f"user_{telegram_id}"
+        short_id = uuid.uuid4().hex[:8]
 
-    # Параметры конфигурации
-    data = {
-        "enable": True,
-        "remark": user_tag,
-        "listen": "",
-        "port": 443,
-        "protocol": "vless",
-        "settings": {
-            "clients": [
-                {
-                    "id": user_uuid,
-                    "flow": "xtls-rprx-vision",
-                    "email": user_tag
+        data = {
+            "enable": True,
+            "remark": user_tag,
+            "listen": "",
+            "port": 443,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": user_uuid,
+                        "flow": "xtls-rprx-vision",
+                        "email": user_tag
+                    }
+                ],
+                "decryption": "none",
+                "fallbacks": []
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "reality",
+                "realitySettings": {
+                    "show": False,
+                    "dest": "yahoo.com:443",
+                    "xver": 0,
+                    "serverNames": ["yahoo.com"],
+                    "fingerprint": "chrome",
+                    "publicKey": "9Hdy5jR2MNBB-vxtu1bOl4SHpiLgTWlEqgDD8ZGf2hk",
+                    "shortId": short_id,
+                    "spiderX": "/"
                 }
-            ],
-            "decryption": "none",
-            "fallbacks": []
-        },
-        "streamSettings": {
-            "network": "tcp",
-            "security": "reality",
-            "realitySettings": {
-                "show": False,
-                "dest": "yahoo.com:443",
-                "xver": 0,
-                "serverNames": ["yahoo.com"],
-                "fingerprint": "chrome",
-                "publicKey": "9Hdy5jR2MNBB-vxtu1bOl4SHpiLgTWlEqgDD8ZGf2hk",
-                "shortId": short_id,
-                "spiderX": "/"
+            },
+            "sniffing": {
+                "enabled": True,
+                "destOverride": ["http", "tls"]
             }
-        },
-        "sniffing": {
-            "enabled": True,
-            "destOverride": ["http", "tls"]
         }
-    }
 
-    resp = await client.post("/xui/inbound/add", json=data)
-    if resp.status_code != 200:
-        raise Exception(f"Ошибка при создании пользователя: {resp.text}")
+        resp = await client.post("/xui/inbound/add", json=data)
+        if resp.status_code != 200:
+            raise Exception(f"Ошибка при создании пользователя: {resp.text}")
 
-    link = (
-        f"vless://{user_uuid}@77.110.103.180:443/?type=tcp"
-        f"&security=reality"
-        f"&pbk=9Hdy5jR2MNBB-vxtu1bOl4SHpiLgTWlEqgDD8ZGf2hk"
-        f"&fp=chrome"
-        f"&sni=yahoo.com"
-        f"&sid={short_id}"
-        f"&spx=%2F"
-        f"&flow=xtls-rprx-vision"
-        f"#{user_tag}"
-    )
+        link = (
+            f"vless://{user_uuid}@77.110.103.180:443/?type=tcp"
+            f"&security=reality"
+            f"&pbk=9Hdy5jR2MNBB-vxtu1bOl4SHpiLgTWlEqgDD8ZGf2hk"
+            f"&fp=chrome"
+            f"&sni=yahoo.com"
+            f"&sid={short_id}"
+            f"&spx=%2F"
+            f"&flow=xtls-rprx-vision"
+            f"#{user_tag}"
+        )
 
-    return link
+        return link
 
 
 @dp.message(Command("get"))
