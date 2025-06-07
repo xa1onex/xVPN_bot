c5a5c7ca980e22ce890470f6d8a3b47e725a347e
macbookair <cr7dima777@gmail.com>
2025-06-07 13:51:31 +0300

    v1.2.4

--- keyboards/inline/app_buttons.py
+++ keyboards/inline/app_buttons.py
@@ -6,15 +6,14 @@ def get_apps_murkup():
     """ Inline buttons для выдачи ссылок на скачивание приложений """
     actions = InlineKeyboardMarkup(row_width=3)
 
-    actions.add(InlineKeyboardButton(text="💻 Windows", url="https://github.com/hiddify/hiddify-next/releases/"
+    actions.add(InlineKeyboardButton(text="Windows", url="https://github.com/hiddify/hiddify-next/releases/"
                                                          "download/v2.0.5/Hiddify-Windows-Setup-x64.exe"),
-                InlineKeyboardButton(text="🍏 MacOS", url="https://apps.apple.com/ru/app/"
-                                                       "hiddify-proxy-vpn/id6596777532"),
-                InlineKeyboardButton(text="🐧 Linux", url="https://github.com/hiddify/hiddify-next/releases/"
+                InlineKeyboardButton(text="MacOS", url="https://apps.apple.com/ru/app/streisand/id6450534064"),
+                InlineKeyboardButton(text="Linux", url="https://github.com/hiddify/hiddify-next/releases/"
                                                        "download/v2.0.5/Hiddify-Debian-x64.deb")
                 )
-    actions.add(InlineKeyboardButton(text="📱 iOS", url="https://apps.apple.com/ru/app/v2raytun/id6476628951"),
-                InlineKeyboardButton(text="🤖 Android", url="https://play.google.com/store/"
+    actions.add(InlineKeyboardButton(text="iOS", url="https://apps.apple.com/ru/app/v2raytun/id6476628951"),
+                InlineKeyboardButton(text="Android", url="https://play.google.com/store/"
                                                          "apps/details?id=app.hiddify.com")
                 )
     actions.add(InlineKeyboardButton(text=_("🔀 Другой вариант"), url="https://github.com/hiddify/"
